06a168c1b96e2c9cdc3d4811fc9078a17f41c69f
raksdewji <rakheemdewji@hotmail.com>
2021-02-13 16:30:02 -0700

    feat: init backend repo

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    quotes: [2, 'single'],
+    semi: [2, 'always'],
+    camelcase: 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+}
