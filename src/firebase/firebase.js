453cb413765a3abe2ea9e33a7448f575b37feeb6
Osagie Omigie <osagieomigie@gmail.com>
2021-02-13 20:02:44 -0700

    setup firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,14 +8,14 @@ admin.initializeApp({
 });
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: 'driven-origin-304722.firebaseapp.com',
-  databaseURL: process.env.databaseURL,
-  projectId: 'driven-origin-304722',
-  storageBucket: 'driven-origin-304722.appspot.com',
-  messagingSenderId: '705209892042',
-  appId: '1:705209892042:web:a1f7366d29e741ced5c833',
-  measurementId: 'G-LHG2R6BPRL'
+  apiKey: "AIzaSyDbNnjOPoUBprERhei--HQK8zCgbqfUhaM",
+  authDomain: "driven-origin-304722.firebaseapp.com",
+  databaseURL: "https://driven-origin-304722-default-rtdb.firebaseio.com",
+  projectId: "driven-origin-304722",
+  storageBucket: "driven-origin-304722.appspot.com",
+  messagingSenderId: "705209892042",
+  appId: "1:705209892042:web:a1f7366d29e741ced5c833",
+  measurementId: "G-LHG2R6BPRL"
 };
 
 const db = admin.firestore();
