b55d82f8fd820a4e55060dec7d5393f1f1b3605e
raksdewji <rakheemdewji@hotmail.com>
2021-02-13 20:10:10 -0700

    style: prettier

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,18 +4,18 @@ const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://driven-origin-304722-default-rtdb.firebaseio.com'
+  databaseURL: 'https://driven-origin-304722-default-rtdb.firebaseio.com',
 });
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbNnjOPoUBprERhei--HQK8zCgbqfUhaM",
-  authDomain: "driven-origin-304722.firebaseapp.com",
-  databaseURL: "https://driven-origin-304722-default-rtdb.firebaseio.com",
-  projectId: "driven-origin-304722",
-  storageBucket: "driven-origin-304722.appspot.com",
-  messagingSenderId: "705209892042",
-  appId: "1:705209892042:web:a1f7366d29e741ced5c833",
-  measurementId: "G-LHG2R6BPRL"
+  apiKey: 'AIzaSyDbNnjOPoUBprERhei--HQK8zCgbqfUhaM',
+  authDomain: 'driven-origin-304722.firebaseapp.com',
+  databaseURL: 'https://driven-origin-304722-default-rtdb.firebaseio.com',
+  projectId: 'driven-origin-304722',
+  storageBucket: 'driven-origin-304722.appspot.com',
+  messagingSenderId: '705209892042',
+  appId: '1:705209892042:web:a1f7366d29e741ced5c833',
+  measurementId: 'G-LHG2R6BPRL',
 };
 
 const db = admin.firestore();
@@ -24,5 +24,5 @@ const auth = firebase.auth();
 
 module.exports = {
   database: db,
-  auth: auth
+  auth: auth,
 };
